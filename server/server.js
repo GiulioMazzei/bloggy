c8d9f62dae342f74e612c9e98deeecf02ee70132
Giulio Mazzei <giulio.mazzei.2005@gmail.com>
2021-07-06 19:52:28 +0200

    changed CORS config

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const app = express()
 //middleware
 
 //cors
-app.use(cors({ origin: 'https://bloggy-webapp.netlify.app/' }));
+app.use(cors({ origin: 'https://bloggy-webapp.netlify.app' })); //omit CORS origin
 
 //parse requests of content-type: application/json
 app.use(bodyParser.json());
