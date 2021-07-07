4f60b21f7bd20372849febf87635a8024f524a1d
Giulio Mazzei <giulio.mazzei.2005@gmail.com>
2021-07-07 11:49:00 +0200

    added env-vars for DB credentials

--- server/db/db.config.js
+++ server/db/db.config.js
@@ -4,10 +4,10 @@ require('dotenv').config();
 //config for the DB
 module.exports = {
 
-    HOST: 'eu-cdbr-west-01.cleardb.com', //process.env.DB_HOST
-    USER: 'b854de145ae131', //process.env.DB_USER
-    PASSWORD: 'b63fd96e', //process.env.DB_PASSWORD
-    DB: 'heroku_9064a3714d3d8e7', //process.env.DB_NAME
+    HOST: process.env.DB_HOST,
+    USER: process.env.DB_USER,
+    PASSWORD: process.env.DB_PASSWORD,
+    DB: process.env.DB_NAME,
     dialect: 'mysql',
     pool: {
       max: 5,
@@ -17,6 +17,3 @@ module.exports = {
     },
     
 };
-  
-
-//mysql://b854de145ae131:b63fd96e@eu-cdbr-west-01.cleardb.com/heroku_9064a3714d3d8e7?reconnect=true
